2ad92d483d4cda97565a8fcf27fdb043e246a97a
Alejandro Gil GÃ³mez <agilgomez@milan1.cm.cluster>
2024-01-19 09:25:28 -0500

    Lecture11

---
+++ Lecture11.R
@@ -0,0 +1,10 @@
+library(diversitree)
+gt <-read.tree('grunts.phy')
+gd<-read.csv("grunts.csv",row.names=1,stringsAsFactors=TRUE)
+
+head(gd)
+hab <-gd[,1]
+names(hab)<-rownames(gd)
+plotTree(gt,offset=0.5)
+tiplabels(pie=to.matrix(hab,0:1)[gt$tip.label,],
+piecol=c("white","black"),cex=0.4)
